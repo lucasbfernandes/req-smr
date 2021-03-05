db3111983787bb8997acf96d669475eb2aec96ea
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2021-03-05 15:04:50 -0300

    fix proxy port
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- proxy/internal/api/api.go
+++ proxy/internal/api/api.go
@@ -1,12 +1,11 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 	"req-smr/internal/constants"
 	"req-smr/internal/usecases"
 )
 
 func StartAPI() {
-	http.ListenAndServe(fmt.Sprintf(":%d", constants.ProxyPort), &usecases.Proxy{})
+	http.ListenAndServe(constants.ProxyPort, &usecases.Proxy{})
 }
