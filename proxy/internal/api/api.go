44b7d3c9287863989ba7cde2a281a10e75b84cd2
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2021-03-05 15:46:09 -0300

    more logs
    Signed-off-by: Lucas Borges Fernandes <lucasbfernandes94@gmail.com>

--- proxy/internal/api/api.go
+++ proxy/internal/api/api.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"req-smr/internal/constants"
 	"req-smr/internal/usecases"
 )
 
 func StartAPI() {
+	fmt.Printf("STEP:START_API_PORT: %s\n", constants.ProxyPort)
 	http.ListenAndServe(constants.ProxyPort, &usecases.Proxy{})
 }
